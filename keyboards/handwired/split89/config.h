a57ddb712415012c7a491d892bfa8cf2496fe19d
Richard Carter <richard@richardcarter.org>
2024-03-20 22:04:04 -0500

    ricket customized split89

--- keyboards/handwired/split89/config.h
+++ keyboards/handwired/split89/config.h
@@ -37,5 +37,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* disable action features */
 //#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
+
+
+/* NKRO by default */
+#define FORCE_NKRO
+
+
+#define DEBOUNCE 5
+#define USB_POLLING_INTERVAL_MS 1
+
+
+
